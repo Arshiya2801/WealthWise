21181f31ddddb7a4cb981dc2120bbe6f7d0c6fa9
Vinay Sisodiya <sisodiya.vinay@example.com>
2025-03-03 14:52:01 +0530

    Initial commit

---
+++ app/(auth)/layout.js
@@ -0,0 +1,8 @@
+import { Divide } from 'lucide-react'
+import React from 'react'
+
+const Authlayout = ({children}) => {
+    return <div className=" flex justify-center pt-40">{children}</div>;
+};
+
+export default Authlayout
